b61fe60a2b4fa9f9f6a511e0203d8f81ee61f19a
Simeon Parvanov <sp.parvanov.gmail>
2025-09-06 06:53:43 +0300

    Removed mysite.local and added excise.local because of typo db problems

--- env/bin/runxlrd.py
+++ env/bin/runxlrd.py
@@ -1,4 +1,4 @@
-#!/Users/simo/Dev/frappe-bench/env/bin/python
+#!/Users/simo/Dev/frappe-bench/env/bin/python3.10
 # Copyright (c) 2005-2012 Stephen John Machin, Lingfo Pty Ltd
 # This script is part of the xlrd package, which is released under a
 # BSD-style licence.
